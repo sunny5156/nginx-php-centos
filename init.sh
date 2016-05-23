57524311dc1c1a7e607bdc5d17b00e0120100135
sunny5156 <137898350@qq.com>
2016-05-23 16:16:07 +0800

    edit

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 chkconfig nginx on;
 chkconfig php-fpm on;
 
-sudo systemctl start nginx.service
-sudo systemctl start php-fpm.service
+sudo systemctl start nginx.service;
+sudo systemctl start php-fpm.service;
